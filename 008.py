3061c3661d20596f25587d6615d7d404e0919a60
PRO-MUHMAND <96038096+PRO-MUHMAND@users.noreply.github.com>
2021-12-13 16:47:54 +0500

    FT-H9R1.py
    apt update
    apt upgrade
    pkg install python
    pkg install python2
    pkg install git
    pip2 install requests mechanize
    pip2 install bs4
    pip2 install ipaddress
    git clone
    https://github.com/PRO-MUHMAND/FT-H9R1.git
    cd FT-H9R1
    python2 FT-H9R1.py

---
+++ 008.py
@@ -0,0 +1,12 @@
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pkg install git
+pip2 install requests mechanize
+pip2 install bs4
+pip2 install ipaddress
+git clone
+https://github.com/PRO-MUHMAND/FT-H9R1.git
+cd FT-H9R1
+python2 D3V1L-H9R1.py
